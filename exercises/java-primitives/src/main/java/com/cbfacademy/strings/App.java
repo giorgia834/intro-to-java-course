1e559bb6ca9a2b6ef4801deb8fcff37a75e8a81e
Giorgia <giorgiashogbanmu@hotmail.it>
2024-11-20 17:21:19 +0000

    format function change

--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -14,7 +14,6 @@ public class App {
     }
 
     public static String format(String item, int quantity, double price) {
-        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[price]. Quantity: [quantity]". The price should be formatted to two decimal places, e.g. 99.99
-        throw new RuntimeException("Not implemented");
+        return String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
     }
 }
