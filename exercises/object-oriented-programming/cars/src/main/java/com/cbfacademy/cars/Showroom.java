5ca6b34f37c17e9f714216e30a5dc4227e12690d
Giorgia <giorgiashogbanmu@hotmail.it>
2024-11-27 21:42:36 +0000

    showroom class

--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/Showroom.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/Showroom.java
@@ -5,8 +5,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Showroom {
-    List<Car> carsInfo;
-    public List<Car> getCars() {
+    private List<Car> carsInfo; /* instantiating the carsInfo variable, stating the data 
+    type will be in the form of Car (Defined in the Car class) */
+    public List<Car> getCars() { /* here we are stating that the type of data returned by getCars 
+        will be in the form defined by the class Car */
         
         Car car1 = new Car("Volvo", "V40", "blue",2012);
         Car car2 = new Car("Porsche", "Panamera", "red", 2009);
