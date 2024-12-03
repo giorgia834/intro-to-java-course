f8fe7ed84bd403a3249e68f066e9550df839dd0f
Giorgia <giorgiashogbanmu@hotmail.it>
2024-12-03 22:38:24 +0000

    flow-control

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -1,12 +1,34 @@
 package com.cbfacademy;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class FlowControlExercises {
 
     public List<String> fizzBuzz(List<Integer> numbers) {
+        
+            List<String> newList = new ArrayList<>();
+
+            for (int number : numbers) { // for number in numbers
+                if (number % 3 == 0 & number % 5 == 0){
+                    newList.add("FizzBuzz");
+                } else if (number % 5 == 0){
+                    newList.add("Buzz");
+
+                }  else if (number % 3 == 0) {
+                    newList.add("Fizz");
+            } else {
+                newList.add(Integer.toString(number));
+            }
+                
+        }
+
+        return newList;
+
+
+
         // TODO - Implement this method such that
         //  it creates a list where for each element of the input list ${numbers}
         //  - if the ${element} is divisible by 3, it adds “Fizz” to the list
@@ -14,14 +36,29 @@ public class FlowControlExercises {
         //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
         //  - it adds the element to the list in any other case
         //  - it returns the constructed list
-        throw new RuntimeException("Not implemented");
+
     }
 
     public String whichMonth(Integer number) {
+             List<Integer> listNumber = new ArrayList<>();
+             Collections.addAll(listNumber, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+
+             List<String> months = new ArrayList<>();
+             Collections.addAll(months, "January,", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+             
+               if (number >= listNumber.size() || number <= 0) {
+                return "Invalid month number";
+               } else {
+                return months.get(number - 1);
+            } 
+
+            
+
+
         // TODO - Implement this method such that
         //  - it returns the month corresponding to the input ${number}
         //  - if the ${number} is invalid, the method should return "Invalid month number"
-        throw new RuntimeException("Not implemented");
     }
 
     public Map<String, Integer> sumOfOddsAndSumOfEvens() {
