f2210c7c895e3168d37f3e82187402823f35fec8
Giorgia <giorgiashogbanmu@hotmail.it>
2024-12-03 19:11:18 +0000

    oop changes

--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
@@ -1,9 +1,11 @@
 package com.cbfacademy.cars;
+// package com.cbfacademy.showroom;
 
 public class App {
     public static void main(String[] args) {
         // TODO: Display details of all cars in showroom
         Showroom values = new Showroom();
-        System.out.println(values.carsInfo);
+        System.out.println(values.getCars());
+        
     }
 }
--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/Showroom.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/Showroom.java
@@ -1,23 +1,26 @@
 package com.cbfacademy.cars;
 
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Showroom {
-    private List<Car> carsInfo; /* instantiating the carsInfo variable, stating the data 
-    type will be in the form of Car (Defined in the Car class) */
+    // public List<Car> carsInfo; /* instantiating the carsInfo variable, stating the data 
+    // type will be in the form of Car (Defined in the Car class) */
     public List<Car> getCars() { /* here we are stating that the type of data returned by getCars 
         will be in the form defined by the class Car */
+        List<Car> showroom = new ArrayList();
+            
         
         Car car1 = new Car("Volvo", "V40", "blue",2012);
         Car car2 = new Car("Porsche", "Panamera", "red", 2009);
         Car car3 = new Car("Audi", "A3", "grey",2018);
-        carsInfo.add(car1);
-        carsInfo.add(car2);
-        carsInfo.add(car3);
+        showroom.add(car1);
+        showroom.add(car2);
+        showroom.add(car3);
         
-        return carsInfo;
+        return showroom;
 
     }
 
@@ -25,7 +28,7 @@ public class Showroom {
 
 // public class Showroom {
 //     public List<Car> getCars() {
-//         List<Integer> carsInfo = new List<>();
+//         List<Integer> showroom = new List<>();
         
 //         carsInfo.add("Volvo", "V40", 2012, "blue");
 //         carsInfo.add("Porsche", "Panamera", 2009, "red");
