06c3b71b8c1cdf595c04ef35428b4f7e944347d6
Giorgia <giorgiashogbanmu@hotmail.it>
2024-12-04 22:26:20 +0000

    updates

---
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/FileExtension.java
@@ -0,0 +1,22 @@
+package com.cbfacademy;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+public class FileExtension {
+
+    public boolean Check(String filename) {
+        String extension = new String();
+        for (int i = 0, i<filename.length, i++) {
+
+        }
+       
+
+    }
+
+    HashMap<String, Integer> map(List<String> filenames) {
+        
+    }
+
+}
---
+++ exercises/java-exceptions/src/main/java/com/cbfacademy/FilenameException.java
@@ -0,0 +1,6 @@
+package com.cbfacademy;
+
+public class FilenameException {
+    
+
+}
