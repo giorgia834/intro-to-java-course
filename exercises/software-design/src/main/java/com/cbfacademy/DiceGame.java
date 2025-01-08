a8ff38b2c442dc3332a18492c352d826514a5e6e
Giorgia <106474194+giorgia834@users.noreply.github.com>
2025-01-08 21:08:15 +0000

    inversion of control task

--- exercises/software-design/src/main/java/com/cbfacademy/DiceGame.java
+++ exercises/software-design/src/main/java/com/cbfacademy/DiceGame.java
@@ -6,8 +6,8 @@ public class DiceGame {
     private int targetScore = 30;
 
     public DiceGame() {
-        player1 = new DicePlayer();
-        player2 = new DicePlayer();
+        player1 = PlayerFactory.create();
+        player2 = PlayerFactory.create();
         player1.setName("Player 1");
         player2.setName("Player 2");
     }
@@ -26,12 +26,12 @@ public class DiceGame {
             System.out.println();
         }
 
-        if(score1 > score2) {
+        if (score1 > score2) {
             winner = player1.getName();
         } else if (score2 > score1) {
             player2.getName();
         }
-        
+
         return winner;
     }
 }
